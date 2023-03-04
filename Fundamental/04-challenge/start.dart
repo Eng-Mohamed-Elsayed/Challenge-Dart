9884f5fb1e333bb774f48576743dca0723d33740
Eng-Mohamed Elsayed <mohamed.elsayed.course@gmail.com>
2023-03-05 00:06:19 +0300

    ADD Challenge 4

---
+++ Fundamental/04-challenge/start.dart
@@ -0,0 +1,23 @@
+void main() {
+  /// Q1: Complete program calculate
+
+  ///  input number 1  & number 2
+  ///  (n1 & n2 : int or double  )
+  ///
+  /// ♦️ check n1 &  n2  if null output 'enter number   Try agin'
+  /// ♦️ check n1 &  n2  if not number  output 'error n1 or n2 just number, try agin'
+  ///
+  /// input operator ( + - * / %  )
+  /// ♦️ check operator if null output 'enter operator  Try agin'
+  ///
+  ///
+  /// create single   method calculate
+  /// 1 - 3 parameter (n1 ,n2 ,op)
+  /// return result ;
+  ///
+  ///  output : ' 7 - 3 = 4 '
+  ///
+
+
+  /// solution
+}
